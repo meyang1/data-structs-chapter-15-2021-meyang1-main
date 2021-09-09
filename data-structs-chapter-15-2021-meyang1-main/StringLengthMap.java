a8b9fa2a93bee577bf85959c0d055cf4db1fa997
meyang1 <meyang@stu.naperville203.org>
2021-09-08 21:47:27 -0500

    Completed StringLengthMap

--- data-structs-chapter-15-2021-meyang1-main/StringLengthMap.java
+++ data-structs-chapter-15-2021-meyang1-main/StringLengthMap.java
@@ -1,12 +1,15 @@
 import java.util.*;
 import java.io.*;
 /**
+ StringLengthMap Program:
+ Max Yang
    Read all words from a file and add them to a
    map whose keys are word lengths and whose values
    are comma-separated strings of words of the same length.
    Then print out those strings, in increasing order by
    the length of their entries.
    Modify Worked Example 15.1.
+  
 */
 
 public class StringLengthMap
@@ -17,40 +20,43 @@ public class StringLengthMap
 
       try (Scanner in = new Scanner(new File(filename)))
       {
-         String temp = "null";
+         String temp = " ";
          Map<Integer, String> wordMap = new TreeMap<>();
         
          while (in.hasNext())
          {
             String word = clean(in.next());
             Integer len = word.length();
-            
+            System.out.println(word);
             
             // Update the map here
             // Modify Worked Example 15.1
             
             temp = wordMap.get(len);
             if(temp == null) {
-                temp = " "; 
-                wordMap.put(len,temp);}
+                temp = " "; //check for first null value
             }
+            
              
-            temp += ", " + word; 
-            wordMap.put(len, temp);
-           
-         }
-         System.out.println(wordMap);
-         
+            temp += " " + word + ","; //add word and comma
+            wordMap.put(len, temp); 
+        }
+        
          // Print the strings, in increasing order of their length
-         // Use this format: 1: i, a, i
-         
+         // Use this format: 1: i, a, i,
+        for (Integer words : wordMap.keySet()) //iterate thru keys/values
+        {
+            System.out.println(words + ":" + wordMap.get(words)); 
+        }
       }
+         
+       
       catch (FileNotFoundException e){
-      {
-         System.out.println("Cannot open: " + filename);
+        {
+            System.out.println("Cannot open: " + filename);
+        }
       }
    }
-
    public static String clean(String s)
    {
       String r = "";
